3b97d2f803496fc612ad1ebeb521891b464e906e
Shubham <ishubhchaudhari@gmail.com>
2023-05-11 17:48:03 +0530

    added day2

---
+++ src/day2/Main.java
@@ -0,0 +1,9 @@
+package day2;
+
+public class Main {
+    public static void main(String[] args) {
+        int myNum = 4;
+        int result = myNum*2;
+        System.out.println(result);
+    }
+}
