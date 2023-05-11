8feb28c679a194d23041a06dc9035ab789f09f02
Shubham <ishubhchaudhari@gmail.com>
2023-05-11 20:51:29 +0530

    added day 3: data types

---
+++ src/day3/Main.java
@@ -0,0 +1,30 @@
+package day3;
+
+public class Main {
+    public static void main(String[] args) {
+
+        boolean isAnimal = true;
+        System.out.println(isAnimal);
+
+        char ch = 'a';
+        System.out.println(ch);
+
+        byte a = 127;
+        System.out.println(a);
+
+        short b = 32767;
+        System.out.println(b);
+
+        int c = 32768;
+        System.out.println(c);
+
+        long d = 10000000000L;
+        System.out.println(d);
+
+        double e =1.1;
+        System.out.println(e);
+
+        float f =1.4f;
+        System.out.println(f);
+    }
+}
