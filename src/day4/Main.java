bc9fe015f771ec86e70906170fb8282b186fd8f3
Shubham <ishubhchaudhari@gmail.com>
2023-05-13 01:53:21 +0530

    added day 4

---
+++ src/day4/Main.java
@@ -0,0 +1,22 @@
+package day4;
+
+public class Main {
+    public static void main(String[] args) {
+        // + - * / %
+        int a =10;
+        int b = a+10;
+        int c = a- 5;
+        int d = a*2;
+        int sum = a + b;
+        System.out.println(sum);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(d);
+        int e = a/5;
+        System.out.println(e);
+        int f = a/6;
+        System.out.println(f);
+        int g = a%6;
+        System.out.println(g);
+    }
+}
