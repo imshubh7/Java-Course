2575627df97544b57ce495dcd091e71b89470ebf
Shubham <ishubhchaudhari@gmail.com>
2023-05-14 01:17:06 +0530

    added day 6

---
+++ src/day6/Main.java
@@ -0,0 +1,18 @@
+package day6;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean a = 5>4;
+        System.out.println(a);
+        a= 5<4;
+        System.out.println(a);
+        a = 4>=4;
+        System.out.println(a);
+        a = 3<=4;
+        System.out.println(a);
+        a = 4==5;
+        System.out.println(a);
+        a = 3!=4;
+        System.out.println(a);
+    }
+}
