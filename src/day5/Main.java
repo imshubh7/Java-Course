ea5a3d366e7d193d28393f230c281808cd6823d4
Shubham <ishubhchaudhari@gmail.com>
2023-05-13 23:58:13 +0530

    added day 5

---
+++ src/day5/Main.java
@@ -0,0 +1,15 @@
+package day5;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        a += 5; //a = a+5
+        System.out.println(a);
+        a -=5;
+        System.out.println(a);
+        a *=5;
+        System.out.println(a);
+        a /=5;
+        System.out.println(a);
+    }
+}
